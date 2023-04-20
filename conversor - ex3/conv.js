17c01a31da03dc7feee66d10f33f43bb7e77d67a
Daniel Carvalho <119054125+danircarv@users.noreply.github.com>
2023-04-20 11:09:16 -0300

    Update conv.js

--- conversor - ex3/conv.js
+++ conversor - ex3/conv.js
@@ -9,36 +9,36 @@ let btnKm = document.getElementById("btn-km");
 
 btnMm.addEventListener("click", function () {
   let num1 = parseFloat(num1Input.value);
-  let resultado = num1 / 1000;
+  let resultado = num1 * 1000;
   alert(resultado + " milímetros");
 });
 
 btnCm.addEventListener("click", function () {
   let num1 = parseFloat(num1Input.value);
-  let resultado = num1 / 100;
+  let resultado = num1 * 100;
   alert(resultado + " centímetros");
 });
 
 btnDm.addEventListener("click", function () {
   let num1 = parseFloat(num1Input.value);
-  let resultado = num1 / 10;
+  let resultado = num1 * 10;
   alert(resultado + " decímetros");
 });
 
 btnDam.addEventListener("click", function () {
   let num1 = parseFloat(num1Input.value);
-  let resultado = num1 * 10;
+  let resultado = num1 / 10;
   alert(resultado + " decâmetros");
 });
 
 btnHm.addEventListener("click", function () {
   let num1 = parseFloat(num1Input.value);
-  let resultado = num1 * 100;
+  let resultado = num1 / 100;
   alert(resultado + " hectômetros");
 });
 
 btnKm.addEventListener("click", function () {
   let num1 = parseFloat(num1Input.value);
-  let resultado = num1 * 1000;
+  let resultado = num1 / 1000;
   alert(resultado + " quilômetros");
 });
